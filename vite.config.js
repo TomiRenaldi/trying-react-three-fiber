13e86997e1597de0cfb121869bb03a5116cc2c4a
TomiRenaldi <tomirenaldi2@gmail.com>
2022-07-26 19:34:07 +0700

    :wrench: Config setup in vite & folder dir

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const dirname = path.resolve()
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build:
+  {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+
   plugins: [react()]
 })
